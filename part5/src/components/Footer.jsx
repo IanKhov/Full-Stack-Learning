fa3d8101c727dbf95353cbcc161fc09bee819351
IanKhov <167153877+IanKhov@users.noreply.github.com>
2026-03-23 04:41:03 +0000

    starting p5

---
+++ part5/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic'
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <p>
+        Note app, Department of Computer Science, By Ian Khov 2026
+      </p>
+    </div>
+  )
+}
+
+export default Footer
